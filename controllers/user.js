f458373ccc1d3bfecc4b454c575680a2262c8e53
Thejusl999 <tj.conquerer@gmail.com>
2024-04-03 20:31:28 +0530

    Task#6 - Your user's password is at risk!!!!

--- controllers/user.js
+++ controllers/user.js
@@ -1,51 +1,71 @@
-const path=require('path');
-const User=require('../models/User');
+const path = require("path");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-exports.signupUser = async (req,res,next)=>{
-    const newUser={
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    };
-    User.create(newUser)
-        .then(result=>{
-            res.status(200).json(newUser);
+exports.signupUser = async (req, res, next) => {
+  const newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  bcrypt.hash(newUser.password, 10, (err, hash) => {
+    if (!err) {
+      User.create({ ...newUser, password: hash })
+        .then((result) => {
+          res.status(200).json(newUser);
         })
-        .catch(err=>{
-           if(err.original.code==='ER_DUP_ENTRY'){
-            res.status(403).json({error:err.original.code});
-           }
+        .catch((err) => {
+          if (err.original.code === "ER_DUP_ENTRY") {
+            res.status(403).json({ error: err.original.code });
+          }
         });
+    }
+  });
 };
 
-exports.connectSignup = async(req, res) => {
-    res.sendFile(path.join(__dirname,'..', 'public','html', 'signup.html'));
+exports.connectSignup = async (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "html", "signup.html"));
 };
 
-exports.loginUser = async (req,res,next)=>{
-    const fetchedUser={
-        email: req.body.email,
-        password: req.body.password
-    };
-    User.findAll({where:{email:fetchedUser.email}})
-        .then(user=>{
-            if(user.length>0){
-                if(fetchedUser.email===user[0].email){
-                    if(fetchedUser.password===user[0].password){
-                        res.status(200).json({response:'User logged in successfully!'});
-                    }else{
-                        res.status(401).json({error:'Incorrect password. Please try again!'});
-                    }
-                }
-            }else{
-                throw new Error();
+exports.loginUser = async (req, res, next) => {
+  const fetchedUser = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  User.findAll({ where: { email: fetchedUser.email } })
+    .then((user) => {
+      if (user.length > 0) {
+        if (fetchedUser.email === user[0].email) {
+          bcrypt.compare(
+            fetchedUser.password,
+            user[0].password,
+            (err, result) => {
+              if (err) {
+                return res.status(500).json({ error: 'Internal server error' });
+              }
+              if (result === true) {
+                return res
+                  .status(200)
+                  .json({ response: "User logged in successfully!" });
+              } else {
+                return res
+                  .status(401)
+                  .json({ error: "Incorrect password. Please try again!" });
+              }
             }
-        })
-        .catch(err=>{
-            res.status(404).json({error:'User not found. Please try again!'});
-        });
+          );
+        }
+      } else {
+        return res
+          .status(404)
+          .json({ error: "User not found. Please try again!" });
+      }
+    })
+    .catch((err) => {
+        return res.status(500).json({ error: 'Internal server error' });
+    });
 };
 
-exports.connectLogin = async(req, res) => {
-    res.sendFile(path.join(__dirname,'..', 'public','html', 'login.html'));
+exports.connectLogin = async (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "html", "login.html"));
 };
