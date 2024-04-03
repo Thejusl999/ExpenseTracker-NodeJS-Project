eee5705a4217467c35bbd16b470bdc24130b945f
Thejusl999 <tj.conquerer@gmail.com>
2024-04-03 19:11:10 +0530

    Task#4 - Creating Login Screen Frontend

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const path=require('path');
 const User=require('../models/User');
 
-exports.addUser = async (req,res,next)=>{
+exports.signupUser = async (req,res,next)=>{
     const newUser={
         name: req.body.name,
         email: req.body.email,
@@ -18,6 +18,34 @@ exports.addUser = async (req,res,next)=>{
         });
 };
 
-exports.connectFrontend = async(req, res) => {
+exports.connectSignup = async(req, res) => {
     res.sendFile(path.join(__dirname,'..', 'public','html', 'signup.html'));
+};
+
+exports.loginUser = async (req,res,next)=>{
+    const fetchedUser={
+        email: req.body.email,
+        password: req.body.password
+    };
+    User.findAll({where:{email:fetchedUser.email}})
+        .then(user=>{
+            if(user.length>0){
+                if(fetchedUser.email===user[0].email){
+                    if(fetchedUser.password===user[0].password){
+                        res.status(200).json({response:'User logged in successfully!'});
+                    }else{
+                        res.status(401).json({error:'Incorrect password. Please try again!'});
+                    }
+                }
+            }else{
+                throw new Error();
+            }
+        })
+        .catch(err=>{
+            res.status(404).json({error:'User not found. Please try again!'});
+        });
+};
+
+exports.connectLogin = async(req, res) => {
+    res.sendFile(path.join(__dirname,'..', 'public','html', 'login.html'));
 };
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const router=express.Router();
 
 const playerController=require('../controllers/user');
 
-router.get('/user/signup',playerController.connectFrontend);
-router.post('/user/signup',playerController.addUser);
+router.get('/user/signup',playerController.connectSignup);
+router.post('/user/signup',playerController.signupUser);
+router.get('/user/login',playerController.connectLogin);
+router.post('/user/login',playerController.loginUser);
 
 module.exports=router;
